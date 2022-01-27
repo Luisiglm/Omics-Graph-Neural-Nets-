c1e87fe48e89bda8a9fc5c406e6ef323d12eb3ae
Luisiglm <63861892+Luisiglm@users.noreply.github.com>
2022-01-27 14:37:15 +0000

    Update layers.py
    Added multi hop gat, gcn and gpool layers.

--- layers.py
+++ layers.py
@@ -239,3 +239,150 @@ class scalar_layer_3d(Layer):
         return(self.activation(tf.math.multiply(inputs, self.w)+self.b))
 
 
+class gcn_k(Layer):
+    """ Graph Convolution Layer adapted to genomic data.
+        Takes as input a tf float 32 object of shape batch x genes x features
+        Initialization Args:
+            adj: a gene x gene numpy array
+            units: the number of features to obtain.
+            activation: a keras activation function.
+        Parameters:
+            w: a features x gat.units  tf variable.
+    """
+    def __init__(self,adj,units, k,activation = None):
+        super(gcn_k, self).__init__()
+        self.id = tf.cast(np.identity(adj.shape[1]), dtype = "float32")
+        self.adj = tf.cast(adj+self.id, dtype ='float32')
+        self.d = tf.math.multiply(self.id,tf.math.reduce_sum(self.adj, axis = 0)**-.5)
+        self.adj_c = self.id-tf.matmul(self.d,tf.matmul(self.adj,self.d))
+        self.activation = tf.keras.activations.get(activation)
+        self.units = units
+        self.nodes = adj.shape[1]
+        self.k = k
+    def build(self, input_shape):
+        w_init = tf.keras.initializers.RandomNormal(  mean=0.0, stddev=1/np.sum(input_shape[-1]), seed=None)
+        self.w = list()
+        self.adjs = list()
+        self.adjs.append(self.id)
+        self.adjs.append(self.adj_c)
+        a = self.adj_c + 0.
+        for i in range(self.k):
+            self.w.append(tf.Variable(name = "weight",
+                             initial_value = w_init(shape = (input_shape[2],self.units), dtype = "float32"),
+                             trainable = True))
+            if i >1:
+                a = tf.matmul(self.adj_c,a)
+                self.adjs.append(a)
+        super().build(input_shape)
+    def call(self, inputs):
+        # batch x nodes x features //inputs
+        for i in range(self.k):
+            if i == 0:      
+                f = tf.matmul(inputs,self.w[i]) # batch x nodes x features'
+            else:
+                h = tf.transpose(inputs,perm=[0, 2, 1])# batch x features' x nodes // 
+                h = tf.matmul(h,self.adjs[i])# message passing.
+                h = tf.transpose(h,perm=[0, 2, 1]) # transpose again!
+                f = tf.add(tf.matmul(h, self.w[i]),f)
+        # transpose this bad boy.
+        return(self.activation(f))# activate and poom!
+
+
+
+class gat_k(Layer):
+    """ Graph Attention Layer adapted to genomic data.
+        Takes as input a tf float 32 object of shape batch x genes x features
+        Initialization Args:
+            adj: a gene x gene numpy array
+            units: the number of features to obtain.
+            activation: a keras activation function.
+        Parameters:
+            w: a features x gat.units  tf variable.
+    """
+    def __init__(self,adj,units, k,activation = None):
+        super(gat_k, self).__init__()
+        self.id = tf.cast(np.identity(adj.shape[1]), dtype = "float32")
+        self.adj = tf.cast(adj+self.id, dtype ='float32')
+        self.adj_abs = tf.math.abs(self.adj)
+        self.d = tf.math.multiply(self.id,tf.math.reduce_sum(self.adj, axis = 0)**-.5)
+        self.adj_c = self.id-tf.matmul(self.d,tf.matmul(self.adj,self.d))
+        self.activation = tf.keras.activations.get(activation)
+        self.units = units
+        self.nodes = adj.shape[1]
+        self.k = k
+    def build(self, input_shape):
+        w_init = tf.keras.initializers.GlorotNormal( seed=None)
+        self.ai = tf.Variable(name = "self_attn",
+                             initial_value = w_init(shape = (self.units,self.nodes), dtype = "float32"),
+                             trainable = True)
+        self.aj = tf.Variable(name = "other_attn",
+                             initial_value = w_init(shape = (self.units,self.nodes), dtype = "float32"),
+                             trainable = True)
+        super().build(input_shape)
+        self.w = list()
+        for i in range(self.k):
+            self.w.append(tf.Variable(name = "weight",
+                             initial_value = w_init(shape = (input_shape[2],self.units), dtype = "float32"),
+                             trainable = True))
+        super().build(input_shape)
+    def call(self, inputs):
+        # batch x nodes x features //inputs
+        for i in range(self.k):
+            if i == 0:      
+                f = tf.matmul(inputs,self.w[i]) # batch x nodes x features'
+                self_attn = tf.matmul(f,self.ai) # batch x nodes x nodes
+                other_attn = tf.transpose(tf.matmul(f,self.aj), perm = [0, 2, 1]) # batch x nodes x nodes
+                attn = tf.math.add(self_attn,other_attn)
+                attn = tf.math.add(attn, -1e09*(1-self.adj_abs))# it should be batch x nodes x nodes
+                attn = tf.nn.softmax(attn, axis = 1)
+                attn = tf.math.multiply(attn, self.adj)# if there are negative nodes!
+                a = attn + 0
+            else:
+                h = tf.transpose(inputs,perm=[0, 2, 1])# batch x features' x nodes // 
+                h = tf.matmul(h,a)# message passing.
+                h = tf.transpose(h,perm=[0, 2, 1]) # transpose again!
+                f = tf.add(tf.matmul(h, self.w[i]),f)
+                a = tf.matmul(a, attn)
+        # transpose this bad boy.
+        return(self.activation(f), attn)# activate and poom!
+
+    
+
+
+class gpool_k(Layer):
+    """ Graph Pooling Layer adapted to genomic data.
+        Takes as input a tf float 32 object of shape batch x genes x features
+        Args:
+            adj: a batch x genes x genes tensor object.
+            units: the number of features to obtain.
+            activation: a keras activation function.
+            w: a features x gat.units  tf variable.
+    """
+    def __init__(self,units, k, activation = None):
+        super(gpool_k, self).__init__()
+        self.activation = tf.keras.activations.get(activation)
+        self.units = units
+        self.k = k
+    def build(self, input_shape):
+        w_init = tf.keras.initializers.GlorotNormal(seed=None)
+        self.w = list()
+        for i in range(self.k):
+            self.w.append(tf.Variable(name = "weight",
+                             initial_value = w_init(shape = (input_shape[2],self.units), dtype = "float32"),
+                             trainable = True))
+        super().build(input_shape)
+    def call(self, inputs, adj):
+        for i in range(self.k):
+            if i == 0:      
+                f = tf.matmul(inputs,self.w[i]) # batch x nodes x features'
+                a = adj + 0.
+            else:
+                h = tf.transpose(inputs,perm=[0, 2, 1])# batch x features' x nodes // 
+                h = tf.matmul(h,a)# message passing.
+                h = tf.transpose(h,perm=[0, 2, 1]) # transpose again!
+                f = tf.add(tf.matmul(h, self.w[i]),f)
+                a = tf.matmul(a,adj)
+        # transpose this bad boy.
+        return(self.activation(f))# activate and poom!
+    
+
