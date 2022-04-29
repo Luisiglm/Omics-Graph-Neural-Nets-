7a0b2b88b6b8214b37e6de9889c9fa9a6733cf17
Luis Iglesias-Martinez <63861892+Luisiglm@users.noreply.github.com>
2022-04-29 14:44:08 +0100

    Added comments for fully_3d and fully_3d_ne

--- layers.py
+++ layers.py
@@ -242,6 +242,10 @@ class gated_pool_ad(Layer):
     
 
 class fully_3d(Layer):
+    """ Fully Connected 3d Tensor Layer.
+        Takes as input a tf float 32 object of shape batch x genes x features.
+        If adds an embedidng for each gene's features. 
+    """
     def __init__(self, units, activation = None):
         super(fully_3d,self).__init__()
         self.activation = tf.keras.activations.get(activation)
@@ -266,3 +270,25 @@ class fully_3d(Layer):
         return(self.activation(tf.matmul(inpts, self.w)+self.b))
 
     
+class fully_3d_ne(Layer):
+     """ Fully Connected 3d Tensor Layer.
+        Takes as input a tf float 32 object of shape batch x genes x features.
+        Unlike, fully_3d, it does not add an embedidng for each gene's features. 
+    """
+    def __init__(self, units, activation = None):
+        super(fully_3d_ne,self).__init__()
+        self.activation = tf.keras.activations.get(activation)
+        self.units = units
+    def build(self, input_shape):
+        w_init = tf.random_normal_initializer()
+        self.w = tf.Variable(name = "kernel",
+                             initial_value = w_init(shape = (input_shape[2],self.units),
+                             dtype = "float32"),
+                             trainable = True)
+        b_init = tf.random_normal_initializer()
+        self.b = tf.Variable(name = "bias",
+                             initial_value = b_init(shape = (self.units,), dtype = "float32"),
+                             trainable = True)
+        super().build(input_shape)
+    def call(self,inputs):
+        return(self.activation(tf.matmul(inputs, self.w)+self.b))
